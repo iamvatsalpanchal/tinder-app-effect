7062dae09d8d4bdc85fa25f0b7f330b11b3c9315
iamvatsalpanchal <vatsalpanchal4488@gmail.com>
2024-04-04 22:46:58 +0530

    script js added

---
+++ script.js
@@ -0,0 +1,60 @@
+var users = [
+    {
+        profilePic: "",
+        displayPic:"https://images.unsplash.com/photo-1508978644997-53cc5bfb8a03?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMwfHxwb3J0cmFpdCUyMGdpcmxzfGVufDB8fDB8fHww",
+        pendingMessage: 4,
+        name: "Vishaka",
+        location: "Ahmedabad",
+        age:21,
+        interest:["Singing","Contnent Creator"],
+        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
+        isFriend: null
+    },
+    {
+        profilePic: "",
+        displayPic:"https://plus.unsplash.com/premium_photo-1668485968590-aff3717c1dbe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODV8fHBvcnRyYWl0JTIwZ2lybHN8ZW58MHx8MHx8fDA%3D",
+        pendingMessage: 5,
+        name: "Anavika",
+        location: "Mumbai",
+        age:22,
+        interest:["Singing","Contnent Creator"],
+        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
+        isFriend: null
+    },
+    {
+        profilePic: "",
+        displayPic:"https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHBvcnRyYWl0JTIwZ2lybHN8ZW58MHx8MHx8fDA%3D",
+        pendingMessage: 2,
+        name: "Maitri",
+        location: "Delhi",
+        age:19,
+        interest:["Singing","Contnent Creator"],
+        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
+        isFriend: null
+    },
+    {
+        profilePic: "",
+        displayPic:"https://images.unsplash.com/photo-1441123694162-e54a981ceba5?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBvcnRyYWl0JTIwY3V0ZSUyMGdpcmxzfGVufDB8fDB8fHww",
+        pendingMessage: 4,
+        name: "Anushka",
+        location: "Bangalore",
+        age:18,
+        interest:["Singing","Contnent Creator"],
+        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
+        isFriend: null
+    },
+
+]
+
+// https://plus.unsplash.com/premium_photo-1668485967900-8ae371817f24?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTYxfHxwb3J0cmFpdCUyMGdpcmxzfGVufDB8fDB8fHww
+
+
+// https://images.unsplash.com/photo-1515130166460-532bc0777f50?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU2fHxwb3J0cmFpdCUyMGdpcmxzfGVufDB8fDB8fHww
+
+// https://images.unsplash.com/photo-1532170579297-281918c8ae72?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fHBvcnRyYWl0JTIwc21pbGUlMjBnaXJsc3xlbnwwfHwwfHx8MA%3D%3D
+
+// https://images.unsplash.com/photo-1623623294233-e320a33b83a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU0fHxwb3J0cmFpdCUyMHNtaWxlJTIwZ2lybHN8ZW58MHx8MHx8fDA%3D
+
+// https://images.unsplash.com/photo-1548142813-c348350df52b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fHBvcnRyYWl0JTIwc21pbGUlMjBnaXJsc3xlbnwwfHwwfHx8MA%3D%3D
+
+// https://plus.unsplash.com/premium_photo-1682095823248-bfc932d1dfd1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
