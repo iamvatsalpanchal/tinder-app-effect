b679b119d199edc1b5a780ffa08815f02d5dab84
iamvatsalpanchal <vatsalpanchal4488@gmail.com>
2024-04-07 18:42:24 +0530

    iamgeChange & setInitial added

--- script.js
+++ script.js
@@ -1,50 +1,89 @@
 var users = [
     {
-        profilePic: "",
+        profilePic: "https://plus.unsplash.com/premium_photo-1668485967900-8ae371817f24?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTYxfHxwb3J0cmFpdCUyMGdpcmxzfGVufDB8fDB8fHww",
         displayPic:"https://images.unsplash.com/photo-1508978644997-53cc5bfb8a03?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMwfHxwb3J0cmFpdCUyMGdpcmxzfGVufDB8fDB8fHww",
         pendingMessage: 4,
         name: "Vishaka",
-        location: "Ahmedabad",
+        location: "Ahmedabad, India",
         age:21,
-        interest:["Singing","Contnent Creator"],
+        // interests:["Singing","Content Creator"],
+        interests:[{
+            icon:`<i class="ri-music-2-fill"></i>`,
+            interest:"music"
+        },
+        {
+            icon:`<i class="ri-quill-pen-fill"></i>`,
+            interest:"writing"
+        },
+    ],
+
         bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
         isFriend: null
     },
     {
-        profilePic: "",
+        profilePic: "https://images.unsplash.com/photo-1623623294233-e320a33b83a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU0fHxwb3J0cmFpdCUyMHNtaWxlJTIwZ2lybHN8ZW58MHx8MHx8fDA%3D",
         displayPic:"https://plus.unsplash.com/premium_photo-1668485968590-aff3717c1dbe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODV8fHBvcnRyYWl0JTIwZ2lybHN8ZW58MHx8MHx8fDA%3D",
         pendingMessage: 5,
         name: "Anavika",
-        location: "Mumbai",
+        location: "Mumbai, India",
         age:22,
-        interest:["Singing","Contnent Creator"],
+        // interests:["Singing","Content Creator"],
+        interests:[{
+            icon:`<i class="ri-music-2-fill"></i>`,
+            interest:"music"
+        },
+        {
+            icon:`<i class="ri-quill-pen-fill"></i>`,
+            interest:"writing"
+        },
+    ],
         bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
         isFriend: null
     },
     {
-        profilePic: "",
+        profilePic: "https://plus.unsplash.com/premium_photo-1682095823248-bfc932d1dfd1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         displayPic:"https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHBvcnRyYWl0JTIwZ2lybHN8ZW58MHx8MHx8fDA%3D",
         pendingMessage: 2,
         name: "Maitri",
-        location: "Delhi",
+        location: "Delhi, India",
         age:19,
-        interest:["Singing","Contnent Creator"],
+        // interests:["Singing","Content Creator"],
+        interests:[{
+            icon:`<i class="ri-music-2-fill"></i>`,
+            interest:"music"
+        },
+        {
+            icon:`<i class="ri-quill-pen-fill"></i>`,
+            interest:"writing"
+        },
+    ],
         bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
         isFriend: null
     },
     {
-        profilePic: "",
+        profilePic: "https://images.unsplash.com/photo-1623623294233-e320a33b83a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU0fHxwb3J0cmFpdCUyMHNtaWxlJTIwZ2lybHN8ZW58MHx8MHx8fDA%3D",
         displayPic:"https://images.unsplash.com/photo-1441123694162-e54a981ceba5?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBvcnRyYWl0JTIwY3V0ZSUyMGdpcmxzfGVufDB8fDB8fHww",
         pendingMessage: 4,
         name: "Anushka",
-        location: "Bangalore",
+        location: "Bangalore, India",
         age:18,
-        interest:["Singing","Contnent Creator"],
+        // interests:["Singing","Content Creator"],
+        interests:[{
+            icon:`<i class="ri-music-2-fill"></i>`,
+            interest:"music"
+        },
+        {
+            icon:`<i class="ri-quill-pen-fill"></i>`,
+            interest:"writing"
+        },
+    ],
         bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus impedit ut sed soluta facilis cum?",
         isFriend: null
     },
 
 ]
+
+
 
 // https://plus.unsplash.com/premium_photo-1668485967900-8ae371817f24?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTYxfHxwb3J0cmFpdCUyMGdpcmxzfGVufDB8fDB8fHww
 
@@ -58,3 +97,72 @@ var users = [
 // https://images.unsplash.com/photo-1548142813-c348350df52b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fHBvcnRyYWl0JTIwc21pbGUlMjBnaXJsc3xlbnwwfHwwfHx8MA%3D%3D
 
 // https://plus.unsplash.com/premium_photo-1682095823248-bfc932d1dfd1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+
+
+let curr = 0
+
+
+function select(elem){
+    return document.querySelector(elem);
+}
+
+// immediate invoked function expression or IIFE
+(function setInitial(){
+    select(".maincard img").src = users[curr].displayPic;
+    select(".incomingcard img").src = users[curr+1]?.displayPic;
+    
+    select(".profileimg img").src = users[curr].profilePic;
+    select(".badge h5").textContent = users[curr].pendingMessage;
+    select(".location h3").textContent = users[curr].location;
+
+    select(".name h1:nth-child(1)").textContent = users[curr].name;
+    select(".name h1:nth-child(2)").textContent = users[curr].age;
+
+
+    let clutter = "";
+    users[curr].interests.forEach(function(interest){
+        clutter += `<div class="tag flex items-center bg-white/30 px-3 rounded-full py-1 gap-3">
+        ${interest.icon}
+        <h5 class="text-sm tracking-tighter capitalize">${interest.interest}</h5>
+      </div>`
+    })
+
+    select(".tags").innerHTML = clutter;
+
+    select(".bio p").textContent = users[curr].bio;
+
+
+
+    
+    curr = 2;
+
+})();
+
+
+function imageChange(){
+    let tl = gsap.timeline();
+
+    tl.to(".maincard",{
+        scale:1.1,
+        opacity:0,
+        ease:Circ,
+        duration:0.9
+    },"a")
+
+    .from(".incomingcard",{
+        scale:0.9,
+        opacity:0,
+        ease:Circ,
+        duration:1.1
+    },"a")
+
+};
+
+
+let deny = select(".deny");
+let accept = select(".accept");
+
+deny.addEventListener("click", function(){
+        imageChange();
+        
+    })
